3154827421472185aa28b526c62b498cc18149dd
William Thomas <user@some-macbook.elon.edu>
2021-01-15 17:25:32 -0500

    initial commit

---
+++ sortfiles.py
@@ -0,0 +1,52 @@
+import argparse, sys
+
+parser = argparse.ArgumentParser(description='Sorts files in a directory into subfolders off their ext.')
+
+parser.add_argument('dir', default='.',
+    help="The directory to sort the files"
+)
+parser.add_argument('--destination', default='.',
+    help='The directory underneath the sorted directory to store the sorted files'
+)
+
+
+args = parser.parse_args()
+print(args)
+
+from os.path import join, exists, isdir, abspath, splitext, basename
+from os import renames, listdir
+
+from multiprocessing import Pool
+
+def sort_file(pathname):
+    """Sorts the path if it is not a directory."""
+    dirname = sys.argv[1]
+    
+    file = join(dirname, pathname)
+    if isdir(file): return
+
+    name, ext = splitext(pathname)
+    base = basename(name)
+
+    # tojoin = []
+    # if tojoin.count('.') > 0: tojoin.remove('.')
+    new = join(dirname, args.destination, ext, pathname)
+    try: renames(file, new)
+    except Exception as e: print(str(e))
+
+def main(dirname):
+    
+    # Check if isdir
+    if not exists(dirname) or not isdir(dirname): 
+        sys.exit("argument given is not a directory")
+    
+    # Exit if unsure
+    response = input(f"Are you sure you want to sort the files in {abspath(dirname)}: ")
+    if response.lower() not in ['yes', 'y']: sys.exit(0)
+
+    files = list(listdir(dirname))
+    with Pool(len(files)) as pool:
+        pool.map(sort_file, files)
+
+if __name__ == '__main__':
+    main(sys.argv[1]) # dirname
