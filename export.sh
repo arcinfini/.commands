f3cbef9c10b54ed8ac518b8d41c58d2b1d833530
William Thomas <user@some-macbook.elon.edu>
2021-01-15 19:33:10 -0500

    update export.sh

--- export.sh
+++ export.sh
@@ -2,7 +2,7 @@
 
 # Gets the directory for this script
 # https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
-DIR=`dirname $0`
+DIR=`dirname $0
 
 for filename in "$DIR"/*.py; do
     filename="${filename##*/}" # removes proceeding directories from name
